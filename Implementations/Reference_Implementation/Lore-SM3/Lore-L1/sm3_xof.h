839d5f3b2c7a237104328032413e2c6662f64349
happyzhao <happyzhao@192.168.1.141>
2026-06-21 23:36:14 +0800

    Shrink SM3 reference XOF state buffer

--- Implementations/Reference_Implementation/Lore-SM3/Lore-L1/sm3_xof.h
+++ Implementations/Reference_Implementation/Lore-SM3/Lore-L1/sm3_xof.h
@@ -7,9 +7,10 @@
 
 #define SM3_XOF128_RATE 168
 #define SM3_XOF256_RATE 136
+#define SM3_XOF128_INPUT_MAX (LORE_SYMBYTES + 2)
 
 typedef struct {
-    uint8_t input[LORE_AUX_INPUT_MAX];
+    uint8_t input[SM3_XOF128_INPUT_MAX];
     size_t inlen;
     size_t outpos;
     uint8_t mode;
